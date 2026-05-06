74c649bc51c7ccf8a211fdafb59bd4d8f79b815b
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-05-07 03:21:28 +0500

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,47 @@
+
+for i in range (1,6):
+    for j in range(i):
+        print("*",end = "")  
+
+    print()        
+
+
+
+for i in range (1,6):
+    for j in range(i):
+        print(j,end = "")  
+
+    print()        
+
+
+for i in range (1,5):
+    for j in range(1,i+1):
+        print(j,end = "")  
+
+    print()        
+
+
+for i in range (1,5):
+    for j in range(1,5):
+        print(j,end = "")  
+
+    print()        
+
+
+
+for i in range (1,5):
+    for j in range(-1,i+1):
+        print(j,end = "")  
+
+    print()
+
+
+
+for i in range (1,5):
+    for j in range(4-i):
+        print("1",end = "")
+    for i in range (2*i-1):
+             print("1",end = "")
+
+     
+    print()  
