848cea6150fa2ed0c8cdfaef681989e50bbe1660
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-05-17 22:48:25 +0500

    Add files via upload

--- Task 1.py
+++ Task 1.py
@@ -1,47 +1,14 @@
+# def hello():
+#     print("Hello World")
+# hello()
 
-for i in range (1,6):
-    for j in range(i):
-        print("*",end = "")  
-
-    print()        
-
-
-
-for i in range (1,6):
-    for j in range(i):
-        print(j,end = "")  
-
-    print()        
+# def info():
+#     print('Name = Latif' ,' ', 'Age = 19')
+# info()
 
 
-for i in range (1,5):
-    for j in range(1,i+1):
-        print(j,end = "")  
-
-    print()        
+# def sum(c, d):
+#     return c+d
+# print(sum(c=1,d=2))
 
 
-for i in range (1,5):
-    for j in range(1,5):
-        print(j,end = "")  
-
-    print()        
-
-
-
-for i in range (1,5):
-    for j in range(-1,i+1):
-        print(j,end = "")  
-
-    print()
-
-
-
-for i in range (1,5):
-    for j in range(4-i):
-        print("1",end = "")
-    for i in range (2*i-1):
-             print("1",end = "")
-
-     
-    print()  
