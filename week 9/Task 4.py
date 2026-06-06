6c4249d39159bb03a2d651f0324987dbd5f2cd1d
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-06-06 11:26:26 +0500

    Add files via upload
    This Week 9 folder contains all week 9 PF lab tasks.

---
+++ week 9/Task 4.py
@@ -0,0 +1,11 @@
+# for i in range(1 , 11):
+#     for j in range(1, 5):
+#         print(i*j ,end = "\t")
+
+#     print()
+
+# for i in range(1 , 5):
+#     for j in range(1, 11):
+#         print(i*j ,end = "\t")
+
+#     print()
---
+++ week 9/Task 5.py
@@ -0,0 +1,16 @@
+# for i in range(1 , 5):
+#     for j in range(4-i):
+#         print(' ' ,end = "")
+#     for i in range(2*i-1 ):   
+#          print("*" ,end = "")
+
+#     print()
+
+
+for i in range(1 , 5):
+    for j in range(4-i):
+        print("     " ,end = "")
+    for i in range(2*i-1 ):   
+         print("1" ,end = "")
+
+    print()
