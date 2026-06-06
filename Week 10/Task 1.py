949e055b5c60be1f554af4a6d1786ae064bc7d9b
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-06-06 11:32:27 +0500

    Add files via upload
    This WEEK 10 folder contains all tasks of week 10 of PF lab.

---
+++ Week 10/Task 1.py
@@ -0,0 +1,14 @@
+# def hello():
+#     print("Hello World")
+# hello()
+
+# def info():
+#     print('Name = Latif' ,' ', 'Age = 19')
+# info()
+
+
+# def sum(c, d):
+#     return c+d
+# print(sum(c=1,d=2))
+
+
---
+++ Week 10/Task 2.py
@@ -0,0 +1,28 @@
+# def sum(c, d):
+#     return c+d
+# c = 1
+# d = 1
+
+# print(sum(c,d))
+
+
+# a=5 
+# b=10
+# print(max(a, b))
+
+
+# a = int(input("Enter any number:"))
+# b = float(input("Enter any number:"))
+# c= input("Enter any number:")
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+def students_info(name, age):
+    print(f"My name is:{name},My roll no is:{age}")
+
+students_info("Latif", 19)
+students_info("Adnan", 19)
+students_info("Ali", 19)
+#print("These r ur's besties")
