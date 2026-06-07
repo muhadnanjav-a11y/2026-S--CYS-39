4f54c65750c650e43aab490acb5b2f496159944b
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-06-07 13:34:38 +0500

    Task 3 .py

--- PF Lab Week 2/Task 3 .py
+++ PF Lab Week 2/Task 3 .py
@@ -1,13 +1,11 @@
 # print pattern of "*"
 n = 5
 
-# Upper 
 for i in range(1, n+1):
     for j in range(1, i+1):
         print("*", end = "")
     print()
 
-# Lower 
 for i in range(n-1, 0, -1):
     for j in range(1, i+1):
         print("*", end = "")
