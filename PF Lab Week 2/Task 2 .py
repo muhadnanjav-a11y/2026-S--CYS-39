d6e09ec701dae36b7032011c4d84429b55288bf2
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-06-07 13:34:07 +0500

    Task 2 .py

--- PF Lab Week 2/Task 2 .py
+++ PF Lab Week 2/Task 2 .py
@@ -12,20 +12,3 @@ for n in range(initial,final+1):
        print(" ",n)
        Sum_prime += n
 print("sum of prime numbers",Sum_prime)
-                            
-# ----------------------------------------------------
-
-# Alternative of prime no .s 
-# Start = int(input("Enter the Starting Number: "))
-# End = int(input("Enter the Ending Number: "))
-
-# print("\n")
-# for n in range(Start, End+1):
-#     j = 1
-#     count = 0
-#     for j in range(j, n+1):
-#         if n % j == 0:
-#             count += 1
-        
-#     if count == 2:
-#         print(" ", n)
