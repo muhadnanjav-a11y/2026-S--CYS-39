fa32975f6441a9e75fc124755adebe6403435a72
muhadnanjav-a11y <muhadnanjav@gmail.com>
2026-06-13 15:12:22 +0500

    Update Adnan.py

--- week_14/Adnan.py
+++ week_14/Adnan.py
@@ -1,5 +1,5 @@
 def welcome():
-    print("Welcome to Badar's Python program!")
+    print("Welcome to Adnan's Python program!")
     print("This program will demonstrate some basic Python features.")
     print("Let's get started!")
 
